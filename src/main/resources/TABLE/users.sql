4e5a99c65af649e7134d8ec63c9cc9f4358ab405
sudong <dodlal123tkd@gmail.com>
2024-09-09 23:56:12 +0900

    [all] feat: sql 쿼리 추가

---
+++ src/main/resources/TABLE/users.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS users; # 기존 테이블 삭제
+
+CREATE TABLE users (
+                       user_id INT AUTO_INCREMENT PRIMARY KEY COMMENT '사용자 ID',
+                       user_email VARCHAR(255) NOT NULL COMMENT '사용자 이메일',
+                       user_name VARCHAR(255) NOT NULL COMMENT '사용자 이름',
+                       password VARCHAR(255) NOT NULL COMMENT '패스워드',
+                       provider varchar(50) NULL COMMENT '소셜 로그인 제공자',
+                       provider_id varchar(255) NULL COMMENT '소셜 로그인 사용 ID',
+                       regi_dtm DATE NOT NULL COMMENT '등록시간',
+                       regi_id DATE NOT NULL COMMENT '등록유저ID',
+                       updt_dtm DATE NOT NULL COMMENT '수정시간',
+                       updt_id DATE NOT NULL COMMENT '수정유저ID'
+);
