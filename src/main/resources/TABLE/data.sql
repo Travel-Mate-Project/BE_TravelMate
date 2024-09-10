6b0c330178ad17e393f859a7f9840f8adcbf9ed6
KimJooHyun <hg116701@naver.com>
2024-09-11 00:57:17 +0900

    Local Mysql Database Setting application.yaml DB password 입력 후 사용 DDL은 schema.sql / DML은 data.sql

---
+++ src/main/resources/TABLE/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (user_email, user_name, password, provider, provider_id, regi_dtm, regi_id, updt_dtm, updt_id)
+VALUES ('example@example.com', '홍길동', 'password123', 'google', '1234567890', '2024-09-11', 1, '2024-09-11', 1);
