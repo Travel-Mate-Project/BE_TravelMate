89f02db235716ff14dd5f01deed89b058d892122
sudong <dodlal123tkd@gmail.com>
2024-09-16 19:33:23 +0900

    [all] feat: globalExceptionHandler 추가

--- src/main/java/com/travelmate/commons/exception/ErrorCode.java
+++ src/main/java/com/travelmate/commons/exception/ErrorCode.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    /** 400 BAD_REQUEST */
+    USER_EMAIL_DUPLICATED_BAD_REQUEST(HttpStatus.BAD_REQUEST, "40000", "다른 이메일을 입력하세요. (중복 이메일)"),
+
     /** 401 UNAUTHORIZED */
     REFRESH_TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED, "40100", "로그인 유효기간이 만료되었습니다. 다시 로그인해주세요.");
-
 
     private final HttpStatus httpStatus;
     private final String code;
